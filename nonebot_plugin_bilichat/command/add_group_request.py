f2ad560aea441a13ef3d3311b89a85e35958ec3c
QuanYX <921195075@qq.com>
2025-03-09 19:54:18 +0800

    :bug: 修复开启关闭指令报错

--- nonebot_plugin_bilichat/command/add_group_request.py
+++ nonebot_plugin_bilichat/command/add_group_request.py
@@ -43,24 +43,25 @@ stop_refuse = on_command(
 
 @add_group_request.handle()
 async def _add_group_request(bot: Bot, event: GroupRequestEvent):
-    logger.info("add_group_request")
-    logger.info(f"event: {event}")
+    logger.debug("add_group_request")
+    logger.debug(f"event: {event}")
     request_user_id = event.get_user_id()
     stranger_info = await bot.call_api("get_stranger_info", user_id = int(request_user_id))
-    logger.info(f"stranger_info: {stranger_info}")
+    logger.debug(f"stranger_info: {stranger_info}")
     level = stranger_info.get("level")
-    logger.info(f"level: {level}")
+    logger.debug(f"level: {level}")
     config = ConfigCTX.get()
     if level and config.refuse.group_id[str(event.group_id)].refuse_level >= level:
         await add_group_request.finish(f"{request_user_id}的等级{level}小于等于{config.refuse.group_id[str(event.group_id)].refuse_level}，拒绝加群请求")
     await add_group_request.finish(f"检测到{request_user_id}的加群请求，请处理")
     
 @start_refuse.handle()
-async def _start_refuse(bot: Bot, event: GroupMessageEvent):
+async def _start_refuse(event: GroupMessageEvent):
     group_id = event.group_id
     config = ConfigCTX.get()
     if not config.refuse.group_id.get(str(group_id), None):
-        config.refuse.group_id[str(group_id)].group_id = str(group_id)
+        config.refuse.group_id[str(event.group_id)] = refuseAddGroupRequestdetailConfig(
+            group_id=str(event.group_id))
         # ConfigCTX.set()
     if config.refuse.group_id[str(group_id)].refuse_group_request:
         await start_refuse.finish("已开启自动拒绝加群请求")
@@ -69,13 +70,15 @@ async def _start_refuse(bot: Bot, event: GroupMessageEvent):
     await start_refuse.finish("已开启自动拒绝加群请求")
 
 @stop_refuse.handle()
-async def _stop_refuse(bot: Bot, event: GroupMessageEvent):
+async def _stop_refuse(event: GroupMessageEvent):
     group_id = event.group_id
     config = ConfigCTX.get()
     if not config.refuse.group_id.get(str(group_id), None):
-        config.refuse.group_id[str(group_id)].group_id = str(group_id)
+        config.refuse.group_id[str(event.group_id)] = refuseAddGroupRequestdetailConfig(
+            group_id=str(event.group_id))
         ConfigCTX.set()
         await stop_refuse.finish("该群未开启自动拒绝加群请求")
     if config.refuse.group_id[str(group_id)].refuse_group_request:
+        config.refuse.group_id[str(event.group_id)].refuse_group_request = False
         await start_refuse.finish("已关闭自动拒绝加群请求")
-    
+    else:await stop_refuse.finish("该群未开启自动拒绝加群请求")
